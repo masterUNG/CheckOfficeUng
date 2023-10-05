c2746b5d7921228cca922c02756bfd188dc723bc
masterUNG <masterUng@abc.com>
2023-10-05 13:27:51 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:checkofficer/states/authen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 void main() {
@@ -14,13 +15,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       home: const Authen(),
-     
       theme: ThemeData(
         useMaterial3: true,
-        // colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.deepOrange),
+        
       ),
     );
   }
@@ -29,7 +30,6 @@ class MyApp extends StatelessWidget {
 class MyHttpOverride extends HttpOverrides {
   @override
   HttpClient createHttpClient(SecurityContext? context) {
-   
     return super.createHttpClient(context)
       ..badCertificateCallback = (cert, host, port) => true;
   }
